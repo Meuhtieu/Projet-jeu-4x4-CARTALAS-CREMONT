5de38dedd2d296b03c7db1e7d835541316d72222
CREMONT <CREMONT@DESKTOP-85BM3JH>
2024-01-26 14:55:13 +0100

    Création du premier dépot

---
+++ src/projet/jeu/pkg4x4/cartalas/cremont/ProjetJeu4x4CARTALASCREMONT.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projet.jeu.pkg4x4.cartalas.cremont;
+
+/**
+ *
+ * @author CREMONT
+ */
+public class ProjetJeu4x4CARTALASCREMONT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
